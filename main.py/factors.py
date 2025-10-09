5d6f53011a33d35e6cc76567bc2af9ce13e494f9
Brime-c <olabarriaga.felipe@gmail.com>
2025-10-08 22:15:36 -0300

    factors 1

---
+++ main.py/factors.py
@@ -0,0 +1,12 @@
+mass_conversion_factors = {
+    "mg": 0.001,
+    "g": 1,
+    "kg": 1000,
+    "oz": 28.3495,
+    "lbs": 453.592,
+}
+
+volume_conversion_factors = {
+    "ml": 0.001,
+    "l": 1,
+}

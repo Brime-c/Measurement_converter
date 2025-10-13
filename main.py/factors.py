64626795459ffe58a3f64aecc9da6d200334a2e4
Brime-c <olabarriaga.felipe@gmail.com>
2025-10-12 22:35:57 -0300

    same unit conversion added

--- main.py/factors.py
+++ main.py/factors.py
@@ -7,6 +7,25 @@ mass_conversion_factors = {
 }
 
 volume_conversion_factors = {
-    "ml": 0.001,
-    "l": 1,
+    "ml": 1,
+    "l": 1000,
+    "cup": 236.588,
+    "gallon_us": 3785.41,
+    "gallon_imperial": 4546.09,
+    "fl_oz_us": 29.5735,
+    "fl_oz_imperial": 28.4131,
+    "us_tbsp": 14.7868,
+    "us_tsp": 4.92892,
 }
+
+length_conversion_factors = {
+    "mm": 0.1,
+    "cm": 1,
+    "inch": 2.54,
+}
+
+temperature_units = [
+    "celsius",
+    "fahrenheit",
+    "kelvin"
+]
